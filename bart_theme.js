6c075d72844b33c83b716b21db9f4627b6166037
David Balmer <david.balmer@transsight.com>
2018-05-10 15:45:25 -0700

    two menus not styled yet

--- bart_theme.js
+++ bart_theme.js
@@ -2,8 +2,17 @@ $(document).ready(function() {
 
   var wasFocused = false;
 
+  $('.topic2').on('mouseover', '.dataset-menu-item', function() { // fade other menu items
+    // $(this).siblings().addClass('unfocused'); // all but "this"
+    $('.dataset-menu-item').addClass('unfocused');
+    $(this).removeClass('unfocused');
+  }).on('mouseout', '.dataset-menu-item', function() { // restore other menu items
+    $('.dataset-menu-item').removeClass('unfocused');
+  });
+
   // show menu on hover in
   $('.topic2').hover(function() {
+    var menu = [];
     var this$ = $(this);
     var height = this$.outerHeight();
     var width = this$.outerWidth();
@@ -11,23 +20,30 @@ $(document).ready(function() {
     var datasets = this$.data().datasets;
 
     var style = {
-      top: - 20 - 2, // - 2 to fix arrow
-      left: width + 20,
-      width: 'auto',
-      minWidth: '10rem',
-      minHeight: height + 40 + 2 // including border
+      left: width + 20, // 20 is arrow width
+      top: - 20 - 2 // - 2 to fix arrow position
     };
 
-    var menu_items = datasets.map(function(dataset) {
+    var menu_items = [];
+
+    _.forEach(datasets, function(dataset, index) {
       var url = '/dataset/' + dataset.name + '/resource/' + dataset.resource_id;
-      return '<li class="dataset-menu-item" data-url="' + url + '">' + dataset.title + '</li>';
+
+      if (index > 4) { // split onto another <ul>
+        menu.push('<ul>' + menu_items.join('') +'</ul>');
+        menu_items = [];
+      }
+
+      menu_items.push('<li class="dataset-menu-item" data-url="' + url + '">' + dataset.title + '</li>')
     });
 
-    $('.topic2').addClass('behind-other-topics');
+    if (datasets.length > 4) {
+      menu.push('<ul>' + menu_items.join('') +'</ul>');
+    }
 
-    $('<div class="dataset-menu"><div class="arrow"><ul>' + menu_items.join('') +'<ul></div></div>').css(style).appendTo(this$);
+    this$.addClass('active').siblings().addClass('behind-other-topics'); // all but "this"
 
-    this$.removeClass('behind-other-topics');
+    $('<div class="dataset-menu"><div class="arrow">' + menu.join('') +'</div></div>').css(style).appendTo(this$);
 
     setTimeout(function() {
       // show dataset preview page on link
@@ -35,6 +51,7 @@ $(document).ready(function() {
         wasFocused = true;
       }, function() {
         wasFocused = false;
+        this$.removeClass('active');
         $('.dataset-menu').remove();
         $('.topic2').removeClass('behind-other-topics');
       }).on('click', 'li', function() {
@@ -44,7 +61,7 @@ $(document).ready(function() {
   }, function() {
     if (!wasFocused) {
       $('.dataset-menu').remove();
-      $('.topic2').removeClass('behind-other-topics');
+      $('.topic2').removeClass('active behind-other-topics');
     }
   });
 
